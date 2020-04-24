27185c4ca4a4edec78dd114eedef9cc1d9cc55af
thauf23 <thauf23@naver.com>
2020-04-24 18:24:05 +0900

    20.04.24.18

--- js/house_detail_page.js
+++ js/house_detail_page.js
@@ -1,46 +1,78 @@
 $(function(){
 // start
-$('.design_color a').on('click',function(e){
-    e.preventDefault();
-    var aIndex = $(this).index();
-    $('.design_color a').removeClass('on');
-    $(this).addClass('on');
-    $('.design_img ul').removeClass('on_block');
-    $('.design_img ul').eq(aIndex).addClass('on_block');
-});
-var imgHeight = $('.design_img ul li img').height();
-console.log(imgHeight);
-var ulHeight = $('.design_img ul').height(imgHeight);
+var imgHeight;
 var nowIndex;
 var carouselLi;
 var liCount;
-var imgWight;
+var imgWidth;
 $(window).resize(function(){
     carousel_setImgPosition();
 });
-// function carouselInit(){
-    $('.design_img ul').height();
-    carouselLi = $('.on_block li');
-    liCount = carouselLi.length;
-    console.log(liCount);
-    nowIndex = 0;
-    carousel_setImgPosition();
-    // carousel();
-// };
-function carousel_setImgPosition(){
-    imgWight = carouselLi.width();
-    console.log(imgWight);
-    for(var i=0; i<liCount; i++){
-        if( i == nowIndex){
-            carouselLi.eq(i).css("left",0);
-        }else{
-            carouselLi.eq(i).css("left",imgWight);
+
+carouselInit()
+// ----------- design_color ---------------------------------
+    $('.design_color a').on('click',function(e){
+        e.preventDefault();
+        var aIndex = $(this).index();
+        $('.design_color a').removeClass('on');
+        $(this).addClass('on');
+        $('.design_img ul').removeClass('on_block');
+        $('.design_img ul').eq(aIndex).addClass('on_block');
+        carouselInit()
+    });
+
+// ----------- design_img_position ---------------------------------
+    function carouselInit(){
+        carouselLi = $('.on_block li');
+        liCount = carouselLi.length;
+        nowIndex = 0;
+        carousel_setImgPosition();
+    };
+    function carousel_setImgPosition(){
+        imgHeight = $('.design_img .on_block li img').height();
+        $('.design_img .on_block').height(imgHeight);
+        imgWidth = carouselLi.width();
+        for(var i=0; i<liCount; i++){
+            if( i == nowIndex){
+                carouselLi.eq(i).css("left",0);
+            }else{
+                carouselLi.eq(i).css("left",imgWidth);
+            }
         }
     }
-}
-// function carousel(){
 
-// }
+// ----------- design_move_prev ---------------------------------
+    $('.design_move p:first').on('click',function(){
+        // console.log(nowIndex);
+        carouselLi.eq(nowIndex).css("left", -imgWidth);
+        if(nowIndex <= 0) {
+            carouselLi.eq(3).css("left", 0);
+            nowIndex = 4;
+        } else {
+            carouselLi.eq(nowIndex - 1).css("left", 0);
+        }
+        nowIndex--;
+        carouselLi.eq(nowIndex + 1).css("left", imgWidth);
+    });
+
+// ----------- design_move_next ---------------------------------
+    $('.design_move p:last').on('click',function(){
+        // console.log(nowIndex);
+        carouselLi.eq(nowIndex).css("left", -imgWidth);
+        if(nowIndex == liCount -1) {
+            carouselLi.eq(0).css("left", 0);
+            nowIndex = -1;
+        } else {
+            carouselLi.eq(nowIndex + 1).css("left", 0);
+        }
+        nowIndex++;
+        carouselLi.eq(nowIndex - 1).css("left", imgWidth);
+    });
+
+// ----------- img_select ---------------------------------
+    $('.design_select button').on('click',function(){
+        $(this).css({background: "#000"});
+    });
 
 // end
 });
